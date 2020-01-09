353384d877e9d8d76d91ef8fa579ed7204f8aac1
Nutthapong Charonekitphichian <nutthapongcharonekitphichian@Nutthapongs-MacBook-Pro.local>
2020-01-09 20:04:22 +0700

    sdsdf

--- home.php
+++ home.php
@@ -1,3 +1,8 @@
-<?php
-    echo 'ไหนอ่ะ.....';
-?>
+<div class='row'>
+    <div class='col-lg-12'>
+        <div class='card shadow'>
+            <div class='card-body'>
+            </div>
+        </div>
+    </div>
+</div>
