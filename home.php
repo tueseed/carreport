c651f4161af475c9df85a3b7cf322eac6ac3692e
Nutthapong Charonekitphichian <nutthapongcharonekitphichian@Nutthapongs-MacBook-Pro.local>
2020-01-12 19:13:06 +0700

    sdsds

--- home.php
+++ home.php
@@ -1,10 +1,8 @@
 <div class='row'>
     <div class='col-lg-12'>
         <div class='card shadow'>
-            <div class='card-header'>
-                <span class='text-success font-weight-bold'>ข้อมูลรถยนต์</span>
-            </div>
             <div class='card-body'>
+            <span class='text-success font-weight-bold'>อนุมัตินโยบาย BCM</span>
             </div>
         </div>
     </div>
