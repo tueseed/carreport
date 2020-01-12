c0e1038b63f0e186655ae2e283998ef1ebe78101
Nutthapong Charonekitphichian <nutthapongcharonekitphichian@Nutthapongs-MacBook-Pro.local>
2020-01-12 19:44:45 +0700

    ด้่าฟนย้่รดกนหอด

--- home.php
+++ home.php
@@ -9,11 +9,71 @@
 </div>
 
 <div class='row mt-3'>
-    <div class='col-lg-12'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1Cq46AsrI_pqgc2wViCFMQ5PuIMGAUveo'>
         <div class='card shadow'>
             <div class='card-body'>
             <span class='text-success font-weight-bold'>วัตถุประสงค์และเป้าหมายความต่อเนื่องทางธุรกิจ</span>
             </div>
         </div>
-    </div>
+    </a>
+</div>
+
+<div class='row mt-3'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1UOjwpsAoQfhZEbqCE58VQPunI9JMoYl8'>
+        <div class='card shadow'>
+            <div class='card-body'>
+            <span class='text-success font-weight-bold'>บันทึกอนุมัติเอกสาร</span>
+            </div>
+        </div>
+    </a>
+</div>
+
+<div class='row mt-3'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1wOs8-X-4pshmLo9nl0bXJ8bm8tQ3CwWI'>
+        <div class='card shadow'>
+            <div class='card-body'>
+            <span class='text-success font-weight-bold'>แต่งตั้งนายทะเบียนเอกสาร</span>
+            </div>
+        </div>
+    </a>
+</div>
+
+<div class='row mt-3'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1gfF6pd1JRMjfTiSq070LQrtPgrCXXpTk'>
+        <div class='card shadow'>
+            <div class='card-body'>
+            <span class='text-success font-weight-bold'>คณะทำงาน BCM</span>
+            </div>
+        </div>
+    </a>
+</div>
+
+<div class='row mt-3'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1g2eO6x9AgaB0EGbcVj52av0ggze9sijI'>
+        <div class='card shadow'>
+            <div class='card-body'>
+            <span class='text-success font-weight-bold'>รายงานผลการตรวจประเมินภายใน</span>
+            </div>
+        </div>
+    </a>
+</div>
+
+<div class='row mt-3'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1n9bEvxg7aQ6zJ1qbyBjTdwooP9CkVGo0'>
+        <div class='card shadow'>
+            <div class='card-body'>
+            <span class='text-success font-weight-bold'>การประชุมทบทวน BCM</span>
+            </div>
+        </div>
+    </a>
+</div>
+
+<div class='row mt-3'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1IPhZDQCgWN2XlNFG9vuDz-WUGeBwyIQe'>
+        <div class='card shadow'>
+            <div class='card-body'>
+            <span class='text-success font-weight-bold'>การอบรม</span>
+            </div>
+        </div>
+    </a>
 </div>
