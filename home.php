72ae8a3797a2a8d960da27928312abd5f1e82aad
Nutthapong Charonekitphichian <tue_seed@hotmail.com>
2020-01-15 14:24:32 +0700

    update

--- home.php
+++ home.php
@@ -1,55 +1,55 @@
 <div class='row'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=17t70fQkQnlBcOHeVytImO3c_eBlr-nD7'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=14_1ceKscPKk3-Be77CkI9_UZB-GBHgRC'>
         <div class='card shadow'>
             <div class='card-body'>
-            <span class='text-success font-weight-bold'>อนุมัตินโยบาย BCM</span>
+            <span class='text-success font-weight-bold'>อนุมัตินโยบาย (อนุมัติ BCM Policy) 2563</span>
             </div>
         </div>
     </a>
 </div>
 
 <div class='row mt-3'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=1Cq46AsrI_pqgc2wViCFMQ5PuIMGAUveo'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1mEvwohIHuRt8J72H5jEzJeVvooz9-Nc4'>
         <div class='card shadow'>
             <div class='card-body'>
-            <span class='text-success font-weight-bold'>วัตถุประสงค์และเป้าหมายความต่อเนื่องทางธุรกิจ</span>
+            <span class='text-success font-weight-bold'>วัตถุประสงค์และเป้าหมายความต่อเนื่องทางธุรกิจขั้นต่ำสุด</span>
             </div>
         </div>
     </a>
 </div>
 
 <div class='row mt-3'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=1UOjwpsAoQfhZEbqCE58VQPunI9JMoYl8'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1LS87alEEuOy2UEFWRNH17VVmj5i4J-7Y'>
         <div class='card shadow'>
             <div class='card-body'>
-            <span class='text-success font-weight-bold'>บันทึกอนุมัติเอกสาร</span>
+            <span class='text-success font-weight-bold'>บันทึกอนุมัติเอกสาร28-3-62</span>
             </div>
         </div>
     </a>
 </div>
 
 <div class='row mt-3'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=1wOs8-X-4pshmLo9nl0bXJ8bm8tQ3CwWI'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1j8c7u7G7T0oAs8qHmyxG8LitlF6KnJTa'>
         <div class='card shadow'>
             <div class='card-body'>
-            <span class='text-success font-weight-bold'>แต่งตั้งนายทะเบียนเอกสาร</span>
+            <span class='text-success font-weight-bold'>แต่งตั้งนายทะเบียนเอกสาร BCM กฟต.1</span>
             </div>
         </div>
     </a>
 </div>
 
 <div class='row mt-3'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=1gfF6pd1JRMjfTiSq070LQrtPgrCXXpTk'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1efRJ76j4ZJ8B8_Nd1X8wAX_2x_i7GMwM'>
         <div class='card shadow'>
             <div class='card-body'>
-            <span class='text-success font-weight-bold'>คณะทำงาน BCM</span>
+            <span class='text-success font-weight-bold'>คำสั่งแต่งตั้งคณะทำงาน BCM กฟต.1 2562</span>
             </div>
         </div>
     </a>
 </div>
 
 <div class='row mt-3'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=1g2eO6x9AgaB0EGbcVj52av0ggze9sijI'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1UDxUjeuCHvGYmzZ5MD6QNAg-zsdGX-DX'>
         <div class='card shadow'>
             <div class='card-body'>
             <span class='text-success font-weight-bold'>รายงานผลการตรวจประเมินภายใน</span>
@@ -59,40 +59,50 @@
 </div>
 
 <div class='row mt-3'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=1n9bEvxg7aQ6zJ1qbyBjTdwooP9CkVGo0'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1ij4S3JtmZ-Pkiuj4HYF2fYeDSJ3f0fys'>
         <div class='card shadow'>
             <div class='card-body'>
-            <span class='text-success font-weight-bold'>การประชุมทบทวน BCM</span>
+            <span class='text-success font-weight-bold'>สรุปรายงานการประชุม MRVต.1ครั้งที่1-2562</span>
             </div>
         </div>
     </a>
 </div>
 
 <div class='row mt-3'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=1IPhZDQCgWN2XlNFG9vuDz-WUGeBwyIQe'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=12ByjH66WlJLIqFotJDMctuuSP1hWLQH1'>
         <div class='card shadow'>
             <div class='card-body'>
-            <span class='text-success font-weight-bold'>การอบรม</span>
+            <span class='text-success font-weight-bold'>BCMS-SD-S1-กฟต.1-001 Rev.01</span>
             </div>
         </div>
     </a>
 </div>
 
 <div class='row mt-3'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=1Zse093A42OdJ_3ua0K7Ni7VU0NBKlTvt'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1n-nJOOLs6Yc8vyTtL4-ayJZ-VYr-oRdS'>
         <div class='card shadow'>
             <div class='card-body'>
-            <span class='text-success font-weight-bold'>แผน BCM</span>
+            <span class='text-success font-weight-bold'>BCMS-ER-S1-กฟต.1-001 แผนตอบโต้ภาวะฉุกเฉิน </span>
             </div>
         </div>
     </a>
 </div>
 
 <div class='row mt-3'>
-    <a class='col-lg-12' href='https://drive.google.com/open?id=1Qi2E1lGlvoA1lTjiRC3IVN6v1qNh8K_h'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=1Ey-A6nX1kIMpigz_xsqGkocvHcQQD-6H'>
         <div class='card shadow'>
             <div class='card-body'>
-            <span class='text-success font-weight-bold'>ฝบส</span>
+            <span class='text-success font-weight-bold'>BCMS-BC-S1-กฟต.1-001 แผน BCP</span>
+            </div>
+        </div>
+    </a>
+</div>
+
+<div class='row mt-3'>
+    <a class='col-lg-12' href='https://drive.google.com/open?id=11ItkyiOqLvduMfDqNzl6SyyFBZBHIuLt'>
+        <div class='card shadow'>
+            <div class='card-body'>
+            <span class='text-success font-weight-bold'>BCMS-RP-S1-กฟต.1-001 แผนการฟื้นฟู กฟต.1</span>
             </div>
         </div>
     </a>
